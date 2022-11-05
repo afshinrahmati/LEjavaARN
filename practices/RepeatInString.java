4fdaca7f5cac681f88ca2a2c1577a3d22db57cfc
Afshin Rahmati <a.rahmati@kianiranian.com>
2022-11-05 15:41:10 +0330

    RepeatInString

---
+++ practices/RepeatInString.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class RepeatInString{
+    public static void main(String[] args){
+    
+    Scanner scanner = new Scanner(System.in);
+
+    String firstString = scanner.next();
+    String secondString = scanner.next();
+    int p = 1;
+    int spilt = secondString.length();
+
+    for(int i = 0 ; i < firstString.length() ; i++){
+       String text =  firstString.substring(i,spilt++);
+
+            System.out.println(text + "==>" + secondString);
+
+        if(text.equals(secondString)){
+            System.out.println(1);
+            p++;
+        }
+    if(spilt >firstString.length()){
+        break;
+    }
+    }
+    
+    System.out.println(p);
+    }
+}
