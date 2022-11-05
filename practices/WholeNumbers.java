8f7026d15f6a7d32dc8e5206b94ec54229a5f3d5
Afshin Rahmati <a.rahmati@kianiranian.com>
2022-11-05 09:08:13 +0330

    whole

--- practices/WholeNumbers.java
+++ practices/WholeNumbers.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
 class WholeNumbers{
     public static void main(String[] args){
         int num = 2;
+
+        Scanner scanner = new Scanner(System.in);
+        int isIt = scanner.nextInt();
+        String messgae = "";
+
       for(int i = 0 ; i < 100 ; i++){
 
         int firstPower = (i+1) - 1;
@@ -9,9 +15,15 @@ class WholeNumbers{
         double multiSeconPower = Math.pow(num, multiSecond);
 
         double t = multiFirstPower * (multiSeconPower-1);
-        System.out.println(t);
+        if(t == isIt){
+            messgae = "YES";
+            break;
+        }else{
+            messgae = "NO";
+        }
 
       }      
+        System.out.println(messgae);
 
 
     }
