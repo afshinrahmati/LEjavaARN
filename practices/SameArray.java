bc72a002c480803608a664c3cc442b771ddef3f3
Afshin Rahmati <a.rahmati@kianiranian.com>
2022-11-05 17:54:07 +0330

    SameArray

--- practices/SameArray.java
+++ practices/SameArray.java
@@ -1,12 +1,16 @@
 class SameArray{
     public static void main(String[] args){
         int[] arr1 = {1,2,3,4};
-        int[] arr2 = {5,3,2,1};
-
+        int[] arr2 = {44,3,2,1};
+        boolean result = true;
         for(int i = 0 ; i < arr1.length ; i++){
-
-            System.out.println(contains(arr1, arr2[i]));
+             result = contains(arr1, arr2[i]);
+            if(!result){
+                break;
+            }
+            
         }
+                System.out.println(result);
  
     }
     public static boolean contains(int[] arr,int v){
